54993657e39e55e374db3f40cdadb77422458a23
mjg1123 <marquisgbusiness@gmail.com>
2019-11-06 23:27:18 -0500

    finished logo setup

---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="logo-main">
+                <img src="/assets/ds_circle_logo.png" alt="daily smarty logo"/> 
+            </div>
+         );
+    }
+}
+ 
+export default Logo;
