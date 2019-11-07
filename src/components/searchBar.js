d2f329e8a2fe0aaea7cddaba331ad3aa3e520f76
mjg1123 <marquisgbusiness@gmail.com>
2019-11-07 01:51:29 -0500

    changed the app to home for some reason?

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import {Field, reduxForm} from "redux-form";
 
 class SearchBar extends Component {
+
+    handleFormSubmit = function ({query}) {
+        console.log("trying to handle submit for query", query);
+    }
+
     state = {  }
+
+    renderInput(field) {
+        return <input type="text" placeholder="Search DaiysSmarty" {...field.input} />
+    }
+
     render() { 
+
+        const {handleSubmit} = this.props;
+
         return ( 
-            <div className="search-bar">
-                <input placeholder ="search" />
-            </div>
+                <form className="search-bar" onSubmit={this.handleFormSubmit.bind(this)}>
+                    <Field name="query" component={this.renderInput} />
+                </form>
          );
     }
 }
+
+SearchBar = reduxForm({
+    form: 'searchBar'
+})(SearchBar);
  
 export default SearchBar;
