9b26fa624ff14e50baa056b428e841e3d2caf969
mjg1123 <marquisgbusiness@gmail.com>
2019-11-11 07:45:33 -0500

    modified post component to render different JSX based on props

--- src/components/post.js
+++ src/components/post.js
@@ -12,17 +12,53 @@ class Post extends Component {
         
     }
 
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                <div className="post-link__box">
+
+                </div>
+                <div className="post-link__link">
+            <a href={post_link.post_url}>useful links{index + 1}</a>
+                </div>
+            </div>
+            )
+        })
+        
+        return links
+    }
+
     render() { 
+       if (this.props.type == 'recent') {
         return ( 
-        <li className ="recent-posts">
-            <div className="recent-posts-title">
-                {this.props.title}
-            </div>
-            <div className="recent-posts_topics">
-                {this.renderTopics()}
-            </div>
-        </li>
-         );
+            <li className ="recent-posts">
+                <div className="recent-posts-title">
+                    {this.props.title}
+                </div>
+                <div className="recent-posts_topics">
+                    {this.renderTopics()}
+                </div>
+            </li>
+             );
+       } else {
+        return (
+            <li className="result-post">
+                <div className="result-post_topics">
+                    {this.renderTopics()}
+                </div>
+                <div className="result-post_title">
+                    {this.props.title}
+                </div>
+                <div className="result-post__links">
+                    {this.renderLinks()}
+                </div>
+            </li>
+        )
+
+       }
+
+        
     }
 }
  
